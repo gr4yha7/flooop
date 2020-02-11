f0292e111d346a4cb72b07d85aba8fc2a1f7ff25
Yur3i <jamaal.ekpenyong22@gmail.com>
2020-02-11 13:58:24 +0100

    added location header

--- flooop/lib/screens/home.dart
+++ flooop/lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -18,20 +19,35 @@ class _HomeScreenState extends State<HomeScreen> {
           SizedBox(width: 5.0),
         ],
       ),
-      drawer: Drawer(
-        child: ListView(
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-            ListTile(
-              leading: Icon(Icons.library_music),
-              title: Text('Music'),
-            ),
+            Text(
+              'Calabar, Canaan City',
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                color: Colors.black.withOpacity(0.6),
+                fontSize: 32.0,
+              ),
+            )
           ],
-        )
+        ),
       ),
+      drawer: Drawer(
+          child: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          ListTile(
+            leading: Icon(Icons.library_music),
+            title: Text('Music'),
+          ),
+        ],
+      )),
     );
   }
 }
